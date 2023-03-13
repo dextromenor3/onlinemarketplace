cef196f43f266d71a1bb3ea4ca8c4dc2ddb21db9
dextromenor3 <vladnov66@gmail.com>
2023-03-13 16:38:58 +0100

    Commit

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your username',
